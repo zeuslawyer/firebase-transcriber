a6d0aa608394f191c0f06c1fbd234921df81b666
Zubin Pratap <zubin.pratap@gmail.com>
2025-01-27 21:47:47 +1100

    update README

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # TODO
 
 - [ ] setup firebase project (https://firebase.google.com/docs/web/setup)
-- [ ] deploy hello world typescrip app to it
+- [ ] npm install -g firebase-tools 
+- [ ] deploy hello world typescript app to it
 - [ ] listen to GCS bucket event
 - [ ] transcribe video or audio at that bucket
 - [ ] save transcription txt to new bucket
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { initializeApp } from "firebase/app";
-import { Analytics, getAnalytics } from "firebase/analytics";
+import { getFunctions, httpsCallable } from "firebase/functions";
+import {
+  getStorage,
+  ref,
+  uploadBytes,
+  StorageReference,
+} from "firebase/storage";
 
 import { firebaseConfig } from "./configs/loadenv";
-
-console.log("LFG! 🔥");
-
-const app = initializeApp(firebaseConfig);
 
 // Check env vars are available
 for (const key in firebaseConfig) {
@@ -13,5 +15,14 @@ for (const key in firebaseConfig) {
     throw Error(`${key} is not defined`);
   }
 }
+console.log("LFG! 🔥");
 
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+const functions = getFunctions(app);
 
+// Create a reference to the bucket you want to monitor
+const bucketRef = ref(storage, "your-bucket-nameTODO");
+
+// Set up a Cloud Function to handle bucket events
+// Note: You'll need to create a corresponding Cloud Function in your Firebase project
